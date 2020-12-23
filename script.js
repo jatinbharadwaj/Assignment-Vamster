ad72fbf2d6f480ed6e2d83a3b92b19160af62b77
Jatin Bhardwaj <31664635+jatinbharadwaj@users.noreply.github.com>
2020-12-23 23:25:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,17 +3,18 @@ var date_of_publication = document.querySelectorAll("div.views-field.views-field
 var Object = document.querySelectorAll('div.views-field.views-field-nothing > span > p:nth-child(6) > font')[0].innerText
 
 var BiddingDate = document.querySelectorAll("div.views-field.views-field-nothing > span:nth-child(1) > span > font ")[3].innerText
-var links = document.querySelectorAll('td > div:nth-child(1) a')
+
 
 
 console.log("Date of Publication:",date_of_publication);
 console.log("Object: ",Object);
 console.log("Bidding Date:",BiddingDate);
 
-for(let i=0;i<links.length;i++)
-{ 
-	console.log(`Link ${i}:`, links[i].href);
-}
+var links = document.querySelectorAll('td > div:nth-child(1) a').forEach((res) => {
+  console.log("Link: ",res.href);
+});
+
+
 
 
 
