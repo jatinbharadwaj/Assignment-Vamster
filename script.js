c37355576f04926837ddc057e797fda04061056f
Jatin Bhardwaj <31664635+jatinbharadwaj@users.noreply.github.com>
2020-12-23 23:01:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-var date_of_publication = document.querySelectorAll("#block-views-block-view-noticia-pbh-block-5 > div > div > div > div > div > div.views-field.views-field-nothing > span > span:nth-child(1) > font > font")[0].innerText
+var date_of_publication = document.querySelectorAll("#block-views-block-view-noticia-pbh-block-5 > div > div.views-field.views-field-nothing > span > span:nth-child(1) > font")[0].innerText
 
-var Object = document.querySelectorAll("document.querySelectorAll("#block-views-block-view-noticia-pbh-block-5 > div > div > div > div > div > div.views-field.views-field-nothing > span > p:nth-child(6) > font > font")[0].innerText")
+var Object = document.querySelectorAll("#block-views-block-view-noticia-pbh-block-5 > div >  div.views-field.views-field-nothing > span > p:nth-child(6) > font")[0].innerText
 
-var BiddingDate = document.querySelectorAll("#block-views-block-view-noticia-pbh-block-5 > div > div > div > div > div > div.views-field.views-field-nothing > span > span:nth-child(19) > font > font")[0].innerText
+var BiddingDate = document.querySelectorAll("#block-views-block-view-noticia-pbh-block-5 > div >div.views-field.views-field-nothing > span > span:nth-child(19) > font ")[0].innerText
 
 var links = document.querySelectorAll("a")
 
 
-console.log(date_of_publication);
-console.log(Object);
-console.log(BiddingDate);
+console.log("Date of Publication:",date_of_publication);
+console.log("Object",Object);
+console.log("Bidding Date",BiddingDate);
 
 for(let i=0;i<links.length;i++)
 { 
-console.log(links[i].href)
+console.log(`Link No${i}`, links[i].href);
 }
 
 
