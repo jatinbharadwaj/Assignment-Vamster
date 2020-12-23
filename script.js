675c98071a4f40d0064010aa0e5e85fac22836f4
Jatin Bhardwaj <31664635+jatinbharadwaj@users.noreply.github.com>
2020-12-23 23:19:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-var date_of_publication = document.querySelectorAll("#block-views-block-view-noticia-pbh-block-5 > div > div.views-field.views-field-nothing > span > span:nth-child(1) > font")[0].innerText
+var date_of_publication = document.querySelectorAll("div.views-field.views-field-nothing > span > span:nth-child(1) > font > font")[0].innerText
 
-var Object = document.querySelectorAll("#block-views-block-view-noticia-pbh-block-5 > div >  div.views-field.views-field-nothing > span > p:nth-child(6) > font")[0].innerText
+var Object = document.querySelectorAll('div.views-field.views-field-nothing > span > p:nth-child(6) > font')[0].innerText
 
-var BiddingDate = document.querySelectorAll("#block-views-block-view-noticia-pbh-block-5 > div >div.views-field.views-field-nothing > span > span:nth-child(19) > font ")[0].innerText
-
-var links = document.querySelectorAll("a")
+var BiddingDate = document.querySelectorAll("div.views-field.views-field-nothing > span:nth-child(1) > span > font ")[3].innerText
+var links = document.querySelectorAll('td > div:nth-child(1) a')
 
 
 console.log("Date of Publication:",date_of_publication);
@@ -13,7 +12,8 @@ console.log("Bidding Date",BiddingDate);
 
 for(let i=0;i<links.length;i++)
 { 
-console.log(`Link No${i}`, links[i].href);
+	console.log(`Link ${i}`, links[i].href);
 }
+
 
 
